a405d6e7d9213038642b006426288f3f99266639
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-06-20 11:42:00 -0300

    Update side-bar.php

--- src/views/includes/side-bar.php
+++ src/views/includes/side-bar.php
@@ -15,9 +15,9 @@
     <hr>
 
     <div class="buttons-menu">
-        <a href="<?= url('comandas') ?>"><i class="fa-solid fa-clipboard"></i><strong>Comandas</strong></a>
-        <a href="link1.htm"><i class="fa-brands fa-product-hunt"></i><strong>Produtos</strong></a>
-        <a href="link1.htm"><i class="fa-solid fa-table-list"></i><strong>Pré-Cadastros</strong></a>
+        <a href="<?= url('/comandas') ?>"><i class="fa-solid fa-clipboard"></i><strong>Comandas</strong></a>
+        <a href="<?= url('/produtos') ?>"><i class="fa-brands fa-product-hunt"></i><strong>Produtos</strong></a>
+        <a href="<?= url('/pre-cadastro') ?>"><i class="fa-solid fa-table-list"></i><strong>Pré-Cadastros</strong></a>
     </div>
 
     <script src="https://kit.fontawesome.com/5dc8345cee.js" crossorigin="anonymous"></script>
